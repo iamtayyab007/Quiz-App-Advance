7bc0833b9a9455ecea64b68c326de1263bea6c1d
iamtayyab007 <tayyabm336@gmail.com>
2025-01-15 15:48:42 +0500

    all files

--- src/Components/Summary.jsx
+++ src/Components/Summary.jsx
@@ -1,54 +1,54 @@
 import React from "react";
-import quizCompletedImage from "../assets/quiz-complete.png";
+import quizCompleteImg from "../assets/quiz-complete.png";
 import Questions from "../questions.js";
-export const Summary = ({ ans }) => {
-  let nullCount = 0;
-  let nullindices = [];
-  const skipped = ans.forEach((e, index) => {
-    if (e === null) {
-      nullCount++;
-      nullindices.push(index);
-    }
-  });
-  let correctQuestions = [];
-  const correct = ans.forEach((e, index) => {
-    if (e === Questions[index].answers[0]) {
-      correctQuestions.push(index);
-    }
-  });
 
+export const Summary = ({ ans }) => {
+  const skipped = ans.filter((skip) => skip === null);
+  const correctAns = ans.filter(
+    (item, index) => item === Questions[index]?.answers?.[0]
+  );
+
+  const skippedAnsShare = Math.round((skipped.length / ans.length) * 100);
+
+  const correctAnsShare = Math.round((correctAns.length / ans.length) * 100);
+
+  const wrongAnsShare = 100 - skippedAnsShare - correctAnsShare;
   return (
     <div id="summary">
-      <img src={quizCompletedImage} alt="Trophy" />
+      <img src={quizCompleteImg} alt="Trophy icon" />
       <h2>Quiz Completed!</h2>
       <div id="summary-stats">
         <p>
-          <span className="number">{nullCount === 0 ? "" : nullCount}</span>
-          {nullindices.map((e) => (
-            <span className="text">
-              <li>{Questions[e].text}</li>
-            </span>
-          ))}
+          <span className="number">{skippedAnsShare}%</span>
+          <span className="text">Skipped</span>
         </p>
         <p>
-          <span className="number">{correctQuestions.length}</span>
-
-          {correctQuestions.map((e) => (
-            <span className="text">{Questions[e].text}</span>
-          ))}
+          <span className="number">{correctAnsShare}%</span>
+          <span className="text">answered correctly</span>
         </p>
-
         <p>
-          <span className="number">10%</span>
+          <span className="number">{wrongAnsShare}%</span>
           <span className="text">answered incorrectly</span>
         </p>
       </div>
       <ol>
-        <li>
-          <h3>2</h3>
-          <p className="question">Question text</p>
-          <p className="user-answer">User's answer</p>
-        </li>
+        {ans.map((answer, index) => {
+          let cssClass = "user-answer";
+          if (answer === null) {
+            cssClass += " skipped";
+          } else if (answer === Questions[index].answers[0]) {
+            cssClass += " correct";
+          } else {
+            cssClass += " wrong";
+          }
+          return (
+            <li key={index}>
+              <h3>{index + 1}</h3>
+              <p className="question">{Questions[index].text}</p>
+              <p className={cssClass}>{answer ?? "skipped"}</p>
+            </li>
+          );
+        })}
       </ol>
     </div>
   );
